244fbe7f2144da37115ecb19f26dc5d4c6347dcd
maximilien-muller <150252400+maximilien-muller@users.noreply.github.com>
2024-11-08 14:49:47 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+"""
+importer sqrt
+"""
+
 from math import sqrt
-
 #### Fonction secondaire
-
-
-def isprime(p):
-
+# isprime calcule si nombre premier
+def isprime(p : int) -> bool :
+    """
+    fonction qui calcule nombre premier
+    """
     # votre code ici
+    if p < 2 :
+        return False
+    for d in range ( 2, int(sqrt(p) +1 )) :
+        if p %d == 0:
+            return False
+    return True
 
-    pass
-
-#### Fonction principale
-
-
+# Fonction principale
 def main():
-
+    """
+    tester la fonction
+    """
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
 
 if __name__ == "__main__":
     main()
